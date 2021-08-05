a747c5ad388578645f2fa5fb2429e6f7336efc21
tamzidpeace <tamjedpeace@gmail.com>
2021-08-05 10:43:51 +0600

    render image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,9 @@ import './car.style.css'
 export const Card = (props) => {
      return (
           <div className="card-container">
-               <h1>{props.monster.name}</h1>
+               <img src={`https://robohash.org/3?set=set${props.monster.id}`} alt="monster" />
+               <h2>{props.monster.name}</h2>
+               <p>{props.monster.email}</p>
           </div>
      )
 }
